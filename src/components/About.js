7d63eaa79fbe4f1ef5c146bad80b6481e867965a
Dan Souza <souza.music@outlook.com>
2022-04-12 12:39:23 -0700

    assignment compelted

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Links from "./links";
 
-function About() {
+function About(props) {
+  if (!props.bio) {
+    return ""
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      
+      <p>{props.bio}</p>
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
